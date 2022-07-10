c3fafce113fe4152969a1a1dbde754ac4c950f45
Viacheslav <80632445+DemaPy@users.noreply.github.com>
2022-07-10 10:22:43 +0200

    Update sidebar.js

--- html constructor/src/class/sidebar.js
+++ html constructor/src/class/sidebar.js
@@ -1,15 +1,38 @@
+import { block } from "../utils"
+import { Text, TitleBlock } from "./block"
+
 export class Sidebar {
-    constructor(selector) {
+    constructor(selector, update) {
         this.el = document.querySelector(selector)
+        this.update = update
+
         this.init()
     }
 
     init() {
         this.el.insertAdjacentHTML('afterbegin', this.template)
+        this.el.addEventListener('submit', this.add.bind(this))
     }
 
     get template() {
-        return `<h1>template</h1>`
+        return [
+            block('text'),
+            block('title')
+        ].join('')
     }
 
+    add(event) {
+        event.preventDefault()
+
+        const type = event.target.name
+        const value = event.target.value.value
+        const styles = event.target.styles.value
+
+        const newBlock = type === 'text' ? new Text (value, {styles: styles}) : new TitleBlock (value, {styles: styles})
+
+        this.update(newBlock)
+
+        event.target.value.value = ''
+        event.target.styles.value = ''
+    }
 }
