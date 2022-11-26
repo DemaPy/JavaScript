eb6f11b97c4a537f02aae54d457247cea73e98fb
Viacheslav <80632445+DemaPy@users.noreply.github.com>
2022-11-26 19:55:11 +0100

    Create patternModule.js

---
+++ patternModule.js
@@ -0,0 +1,30 @@
+// Pattern Module
+
+const myModule = (() => {
+    let _name = 'Viacheslav'
+    function setName(n) {
+        if (n) {
+            _name = n
+        }
+        console.log('Name has been changed to')
+    }
+
+    function getName() {
+        console.log(_name)
+    }
+
+    return {
+        changeName: (name) => {
+            setName(name)
+        },
+        getName: () => {
+            getName()
+        }
+    }
+})()
+
+myModule.
+
+myModule.getName()
+myModule.changeName('Dema')
+myModule.getName()
